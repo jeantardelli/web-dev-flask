8a8f4dcc58db6a98c37bc730f90f2a4e8baa452f
jeantardelli <jeantardelli@gmail.com>
2020-11-09 22:01:50 +0100

    custom error pages (3d)

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
